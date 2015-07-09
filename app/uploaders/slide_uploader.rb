7e20483bcbb7b95ae53f35ab65ae6ee51c6b3486
Dmitry Jakovlev <Jakovlev.DS@gmail.com>
2015-07-09 23:19:31 +0700

    Add img at right of the slider

--- app/uploaders/slide_uploader.rb
+++ app/uploaders/slide_uploader.rb
@@ -33,7 +33,7 @@ class SlideUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [1000, 300]
+    process :resize_to_fill => [720, 280]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
